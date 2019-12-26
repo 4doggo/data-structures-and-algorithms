457356201ccba7f50012b3a1471812d229a5d2f0
shingoed <nakajima.shingo@gmail.com>
2019-12-25 23:47:40 -0800

    fixed it and test running

--- code-challenges/twodimentionalarrays/challenges-12.test.js
+++ code-challenges/twodimentionalarrays/challenges-12.test.js
@@ -2,10 +2,13 @@
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1
-You friend Pat has a chain of stores around the greater Seattle area. He specializes in selling salmon cookies. Pat has data for the hourly sales of cookies per hour for each store. He wants to create an array of the total number of cookies sold per hour for all of his stores 
-combined.
+
+You friend Pat has a chain of stores around the greater Seattle area. He specializes in selling salmon cookies. Pat has data for the hourly sales of cookies per hour for each store. He wants to create an array of the total number of cookies sold per hour for all of his stores combined.
+
 Write a function named grandTotal that adds up the cookies sales for each hour of operation for all of the stores combined. For example, the first element in the hourlySales array should be the sum of the cookies sold in the 9:00 a.m. hour at all five stores combined.
+
 For this example, the total at 9:00 a.m. is 17 + 26 + 7 + 5 + 33, or 88 total cookies.
+
 Return the array of the total number of cookies sold per hour for all of the stores combined.
 ------------------------------------------------------------------------------------------------ */
 
@@ -20,36 +23,27 @@ const alkiBeach = [33, 31, 147, 130, 27, 93, 38, 126, 141, 63, 46, 17];
 const cookieStores = [firstPike, seaTac, seattleCenter, capHill, alkiBeach];
 
 const grandTotal = (stores) => {
-  let hourlySales = [];
-  for (let i = 0; i < stores[0].length; i++) {
-    let sum = 0;
-    for (let j = 0; j < stores.length; j++) {
-      sum = sum + stores[j][i];
-    }
-    hourlySales[i] = sum;
-  }
-  return hourlySales;
+  // Solution code here...
+
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
+
 Pat has decided that he would also like to organize his data as objects containing the number of cookies sold per hour and the time.
+
 Here is sample data for the 9:00 sales: { sales: '88 cookies', time: '9 a.m.' }.
-Write a function named salesData that uses forEach to iterate over the hourlySales array and create an object for each hour. 
-Return an array of the formatted data.
+
+Write a function named salesData that uses forEach to iterate over the hourlySales array and create an object for each hour. Return an array of the formatted data.
 ------------------------------------------------------------------------------------------------ */
 
 const salesData = (hours, data) => {
-  let sales = [];
-  for (let i = 0; i < hours.length; i++) {
-    let newObject = { sales: data[i] + ' cookies', time: hours[i], };
-    sales.push(newObject);
-  }
-  return sales;
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
+
 Write a function named howManyTreats that will return the quantity of treats you need to pick up from the pet store today from this array.
 ------------------------------------------------------------------------------------------------ */
 
@@ -69,21 +63,16 @@ const errands = [
 ];
 
 const howManyTreats = (arr) => {
-  let number = 0;
-  arr.forEach(store => {
-    store.items.forEach(item => {
-      if (item.name === 'Treats') {
-        number = item.quantity;
-      }
-    });
-  });
-  return number;
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
+
 Write a function named battleship that accepts a 2D array and two numbers: a row coordinate and a column coordinate.
+
 Return "hit" or "miss" depending on if there's part of a boat at that position in the array. Assume the array has only one of two values at each index. '#' for part of a boat, or ' ' for open water.
+
 Here is a sample board:
 [
   ['#', ' ', '#', ' '],
@@ -91,38 +80,32 @@ Here is a sample board:
   ['#', ' ', ' ', ' '],
   [' ', ' ', '#', '#'],
 ]
+
 The top row of the board is considered row zero and row numbers increase as they go down.
 ------------------------------------------------------------------------------------------------ */
 
 const battleship = (board, row, col) => {
-  if (board[row][col] === '#') {
-    return 'hit';
-  } else if (board[row][col] === ' ') {
-    return 'miss';
-  }
+  //  Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
+
 Write a function named calculateProduct that takes in a two-dimensional array of numbers, multiplies all of the numbers in each array, and returns the final product. This function should work for any number of inner arrays.
+
 For example, the following input returns a product of 720: [[1,2], [3,4], [5,6]]
 ------------------------------------------------------------------------------------------------ */
 
 const calculateProduct = (numbers) => {
-  let product = numbers[0][0];
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < numbers[i].length; j++) {
-      product *= numbers[i][j];
-    }
-  }
-  return product;
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 6
+
 Write a function named averageDailyTemperature that accepts a two-dimensional array representing average daily temperatures grouped week-by-week.
-Calculate the average daily temperature during that entire period. Your output should be a single number. Write your function so it could 
-accept an array with any number of weeks given to it.
+
+Calculate the average daily temperature during that entire period. Your output should be a single number. Write your function so it could accept an array with any number of weeks given to it.
 ------------------------------------------------------------------------------------------------ */
 
 // Real daily average temperatures for Seattle, October 1-28 2017
@@ -130,25 +113,20 @@ const weeklyTemperatures = [
   [66, 64, 58, 65, 71, 57, 60],
   [57, 65, 65, 70, 72, 65, 51],
   [55, 54, 60, 53, 59, 57, 61],
-  [65, 56, 55, 52, 55, 62, 57]
+  [65, 56, 55, 52, 55, 62, 57],
 ];
 
 const averageDailyTemperature = (weather) => {
-  let sum = 0;
-  let counter = 0;
-  for (let i = 0; i < weather.length; i++) {
-    for (let j = 0; j < weather[i].length; j++) {
-      sum += weather[i][j];
-      counter++;
-    }
-  }
-  return sum / counter;
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 7
+
 Write a function named lowestWeeklyAverage that accepts a two-dimensional array of daily temperatures grouped week-by-week.
+
 Calculate the average temperature for each week and return the value of the lowest weekly average temperature.
+
 For example, in the data set below, the lowest weekly average is 46, which is the average of the temperatures in week 2. All other weeks have average temperatures that are greater than 46.
 ------------------------------------------------------------------------------------------------ */
 
@@ -160,65 +138,34 @@ let lowestWeeklyTemperatureData = [
 ];
 
 const lowestWeeklyAverage = (weather) => {
-  let counter = 0;
-  let avg = 0;
-  let avgs = [];
-  let sum = 0;
-
-  for (let i = 0; i < weather.length; i++) {
-    sum = 0;
-    counter = 0;
-    for (let j = 0; j < weather[i].length; j++) {
-      sum += weather[i][j];
-      counter++;
-    }
-    avg = sum / counter;
-    avgs.push(avg);
-  }
-  avgs.sort((a, b) => {
-    return a - b;
-  });
-  return avgs[0];
+  // Solution code here...
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 8
+
 Write a function called excel that accepts a string representing rows and columns in a table.
-Rows are separated by newline "\n" characters. Columns are separated by commas. For example, '1,1,1\n4,4,4\n9,9,9' represents a 3x3 table.
-The function should parse the string as rows and columns and compute the sum of the values for each row. Return an array with the sum of the 
-values in each row.
+
+Rows are seperated by newline "\n" characters. Columns are seperated by commas. For example, '1,1,1\n4,4,4\n9,9,9' represents a 3x3 table.
+
+The function should parse the string as rows and columns and compute the sum of the values for each row. Return an array with the sum of the values in each row.
+
 For example, excel('1,1,1\n4,4,4\n9,9,9') returns [3, 12, 27].
 ------------------------------------------------------------------------------------------------ */
 
 const excel = (str) => {
-  str = str.split('\n')
-  let matrix = [[]];
-  for (let i = 0; i < str.length; i++) {
-    let arr = str[i].split(',')
-    // console.log('arr is', arr)
-    for (let j = 0; j < arr[i].length; j++) {
-      parseInt(arr[i][j])
-      matrix[i].push(arr[i][j])
-      // console.log(arr[i][j])
-    }
-    console.log(arr)
-  }
+  // Solution code here...
 };
-
-let str = '1,1,1\n4,4,4\n9,9,9';
-// let regex = /\\n/g;
-//   str = str.split('\n')
-//   console.log(str)
-
-
-// console.log(str)
-// excel(str) // returns [3, 12, 27]
 
 /* ------------------------------------------------------------------------------------------------
 TESTS
+
 All the code below will verify that your functions are working to solve the challenges.
+
 DO NOT CHANGE any of the below code.
+
 Run your tests from the console: jest challenge-12.test.js
+
 ------------------------------------------------------------------------------------------------ */
 
 
@@ -301,7 +248,7 @@ describe('Testing challenge 7', () => {
   });
 });
 
-xdescribe('Testing challenge 8', () => {
+describe('Testing challenge 8', () => {
   test('It should return the total count for each row', () => {
     let result = excel('1,1,1\n4,4,4\n9,9,9');
     expect(result.length).toStrictEqual(3);
